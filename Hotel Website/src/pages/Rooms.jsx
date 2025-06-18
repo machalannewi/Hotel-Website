846b5f4268b59ee64a9e4a2d51926fde7e28a1a3
Daniel Okeke <danielekene68@gmail.com>
2025-06-18 14:00:41 +0100

    update

--- Hotel Website/src/pages/Rooms.jsx
+++ Hotel Website/src/pages/Rooms.jsx
@@ -15,11 +15,9 @@ export default function Rooms() {
     <Header />
     <div className="min-h-screen">
       {/* Hero Banner */}
-      <div 
-        className="bg-cover bg-center h-64 flex items-center justify-center"
-      >
-        <div className="relative bg-black bg-opacity-50 w-full h-full flex items-center justify-center bg-[url('/images/woman-talking-phone-front-view.jpg')]">
+      <div className="relative bg-cover bg-center h-64 flex items-center justify-center bg-[url('/images/woman-talking-phone-front-view.jpg')]">
         <div className="absolute inset-0 bg-black/50"></div>
+         <div className="relative bg-black bg-opacity-50 w-full h-full flex items-center justify-center">
           <h1 className="relative text-4xl font-bold text-white">Our Rooms</h1>
         </div>
       </div>
